ca20db39da5318ef251e7abcd9cf01eb7d18071e
strongest-qiang <1309148358@qq.com>
2024-10-27 15:40:56 +0800

    fix: 去除重复的.login_view类

--- socket_io_front/src/views/LoginView.vue
+++ socket_io_front/src/views/LoginView.vue
@@ -2,7 +2,7 @@
  * @Author: strongest-qiang 1309148358@qq.com
  * @Date: 2024-10-20 14:43:15
  * @LastEditors: strongest-qiang 1309148358@qq.com
- * @LastEditTime: 2024-10-23 09:28:22
+ * @LastEditTime: 2024-10-27 15:38:03
  * @FilePath: \Front-end\Vue\Vue3\IM\socket_io\socket_io_front\src\views\LoginView.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -31,16 +31,6 @@ function toggle() {
     </div>
 </template>
 <style scoped>
-.login_view {
-    width: 100%;
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    background: url('@/assets/img/login_background.jpg') no-repeat;
-}
-
 .login_view {
     display: flex;
     align-items: center;
