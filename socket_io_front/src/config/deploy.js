9f48e2b8d444ccdfad7522aae60d99047d1168b9
qiang <1309148358@qq.com>
2024-11-09 12:18:31 +0800

    feat:新增文档说明，对socket进行了调整，修复信息重复收到

--- socket_io_front/src/config/deploy.js
+++ socket_io_front/src/config/deploy.js
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export const deployConfig = {
-  host: "121.43.58.18", //远程服务器的ip地址,请用自己的(上面的ip地址是乱写的)
+  host: "121.13.18.28", //远程服务器的ip地址,请用自己的(上面的ip地址是乱写的)
   username: "root", //远程服务器的连接用户，请用自己的(上面的username是乱写的)
-  password: "Xx110120.", //远程服务器的密码，请用自己的(上面的username是乱写的)
+  password: "12saX1.", //远程服务器的密码，请用自己的(上面的username是乱写的)
 };
